82a3979c41c0fdede06c9ab6eb095bc80ea34ab1
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-05-25 15:42:55 -0500

    Created functionality for quiz along with questions array and linking to correect HTML elements. TODO next: Create functionality for timer function and add local storage functionality

--- Coding_Quiz_Challenge/script.js
+++ Coding_Quiz_Challenge/script.js
@@ -1,10 +1,5 @@
 
 
-
-
-
-
-
 var quizQuestions = [
   {
     question: "Question 1: What is the capital of France?",
@@ -33,3 +28,69 @@ var quizQuestions = [
   }
 ];
 
+var currentQuestionIndex = 0;
+var score = 0;
+
+function startQuiz() {
+  displayQuestion();
+  startTimer();
+}
+
+function displayQuestion() {
+  var questionPrompt = document.getElementById("question-prompt");
+  var choicesList = document.getElementsByClassName("choices")[0];
+  
+  questionPrompt.textContent = quizQuestions[currentQuestionIndex].question;
+  
+  choicesList.innerHTML = "";
+  
+  for (var i = 0; i < quizQuestions[currentQuestionIndex].choices.length; i++) {
+    var choice = quizQuestions[currentQuestionIndex].choices[i];
+    var li = document.createElement("li");
+    li.innerHTML = "<a href='#'>" + choice + "</a>";
+    li.id = "choiceInput" + (i + 1);
+    li.addEventListener("click", checkAnswer);
+    choicesList.appendChild(li);
+  }
+}
+
+function checkAnswer(event) {
+  var selectedChoice = event.target.parentNode.id;
+  var choiceIndex = parseInt(selectedChoice.replace("choiceInput", ""));
+  
+  if (choiceIndex === quizQuestions[currentQuestionIndex].correctAnswer) {
+    score++;
+    displayResult("Correct!", "green");
+  } else {
+    displayResult("Wrong!", "red");
+  }
+  
+  currentQuestionIndex++;
+  
+  if (currentQuestionIndex < quizQuestions.length) {
+    displayQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+function displayResult(message, color) {
+  var resultElement = document.getElementsByClassName("result")[0];
+  
+  resultElement.textContent = message;
+  resultElement.style.color = color;
+}
+
+function endQuiz() {
+  stopTimer();
+  alert("Quiz has ended! Your score is: " + score);
+}
+
+function startTimer() {
+}
+
+function stopTimer() {
+}
+
+var startButton = document.getElementById("start-quiz");
+startButton.addEventListener("click", startQuiz())
