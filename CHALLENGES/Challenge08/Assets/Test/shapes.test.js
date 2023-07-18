f3fdff154561a8a121a197dd5d523c53530e729b
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-07-17 23:26:55 -0500

    got shape test working properly

--- CHALLENGES/Challenge08/Assets/Test/shapes.test.js
+++ CHALLENGES/Challenge08/Assets/Test/shapes.test.js
@@ -5,26 +5,27 @@ const triangle = new Triangle();
 
 
 describe("Square", () => {
-    test("Render svg of blue square"), () => {
-        square.setColor('blue')
-        const expectedSquare = '<rect x="90" y="40" width="120" height="120" fill="blue"/>'
-        const squareRender = square.render()
-        expect(squareRender).toEqual(expectedSquare)
-    }
+    test("Render svg for a blue square", () => {
+      const expectedSquare =
+        '<rect x="90" y="40" width="120" height="120" fill="blue" />';
+      square.setColor("blue");
+      const renderSquare = square.render();
+      expect(renderSquare).toEqual(expectedSquare);
+    });
 })
 describe("Circle", () => {
-    test("Render svg of purple circle"), () => {
-        circle.setColor('purple')
-        const expectedcircle = '<circle cx="150" cy="100" r="80" fill="purple"/>'
-        const circleRender = circle.render()
-        expect(circleRender).toEqual(expectedcircle)
-    }
+    test("Render svg of purple circle", () => {
+        const expectedCircle = '<circle cx="150" cy="100" r="80" fill="purple" />';
+        circle.setColor("purple");
+        const renderCircle = circle.render();
+        expect(renderCircle).toEqual(expectedCircle);
+    })
 })
 describe("Triangle", () => {
-    test("Render svg of red Triangle"), () => {
+    test("Render svg of red Triangle", () => {
         triangle.setColor('red')
-        const expectedTriangle = '<polygon points="150, 18 244, 182 56, 182" fill="red"/>'
+        const expectedTriangle = '<polygon points="150, 18 244, 182 56, 182" fill="red" />'
         const triangleRender = triangle.render()
         expect(triangleRender).toEqual(expectedTriangle)
-    }
+    })
 })
